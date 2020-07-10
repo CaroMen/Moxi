34247508140287f9c74d5460d2a20957684d9338
Caroline <caroline_mendez@baylor.edu>
2020-07-10 15:55:40 -0500

    Moxi

--- Accounts/urls.py
+++ Accounts/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home),
+    path('', views.home, name="home"),
     path('signup/', views.signup, name="signup"),
     path('calendar/', views.cal),
 
