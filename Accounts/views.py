a38e4c52e7e9fa4b54ec507ceacd5383ce9f59a1
Caroline <caroline_mendez@baylor.edu>
2020-07-11 15:34:07 -0500

    Moxi

--- Accounts/views.py
+++ Accounts/views.py
@@ -12,5 +12,9 @@ def signup(request):
     return render(request, 'accounts/signup.html')
 
 
+def signin(request):
+    return render(request, 'accounts/signin.html')
+
+
 def cal(request):
     return render(request, 'accounts/calendar.html')
